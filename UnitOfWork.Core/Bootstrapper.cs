b3887472c51686eea8a1867c5e88818d76ec82bf
Coderrob <Rob@coderrob.com>
2016-03-07 14:26:04 -0500

    (update) Adds core project to add concept of Bootstrapper for DI registration of datacontext and Repository objects.

---
+++ UnitOfWork.Core/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Practices.Unity;
+using UnitOfWork.Data;
+using UnitOfWork.Data.DataContext;
+using UnitOfWork.Data.Repository.EntityFramework;
+using UnitOfWork.Data.UnitOfWork;
+using EF = UnitOfWork.Data.UnitOfWork.EntityFramework;
+
+namespace UnitOfWork.Core
+{
+    public class Bootstrapper
+    {
+        public IUnityContainer RegisterTypes(IUnityContainer container)
+        {
+            return container
+                .RegisterType<IDataContext, DataContext>(new HierarchicalLifetimeManager(), new InjectionConstructor("DatabaseContext"))
+                .RegisterType<IUnitOfWork, EF.UnitOfWork>(new HierarchicalLifetimeManager())
+                .RegisterType(typeof (IRepository<>), typeof (Repository<>))
+                ;
+        }
+    }
+}
